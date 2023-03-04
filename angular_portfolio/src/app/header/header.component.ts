7514cd10b9d3cfcb4308e65b6f3e4887ae9381b6
MM12300 <nerggalkore@gmail.com>
2023-03-04 14:55:56 +0100

    header close details on click

--- angular_portfolio/src/app/header/header.component.ts
+++ angular_portfolio/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Photo} from "../interfaces/photo";
 import {PhotosService} from "../services/photos.service";
 
@@ -9,11 +9,17 @@ import {PhotosService} from "../services/photos.service";
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    @ViewChild('details') details : ElementRef;
+
     public categories: string[];
 
     constructor(private _photoService: PhotosService) {
         this._photoService.getPhotos("home").subscribe((res: Photo[]) => {
             this.categories = res.map(photo => photo.category);
         })
+    }
+
+    public closeDetails(): void{
+        this.details.nativeElement.removeAttribute("open");
     }
 }
