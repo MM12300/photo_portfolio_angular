5b731d37eabd918e1e8446a612a21e6c7c591891
MM12300 <nerggalkore@gmail.com>
2023-03-06 20:40:58 +0100

    api key issue, public portfolio -> key blocked, api key served : cors issue. Can't get a clean way to do it for now.

--- angular_portfolio/src/app/services/instagram.service.ts
+++ angular_portfolio/src/app/services/instagram.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {firstLongLifeToken} from "../../../secrets/secrets";
 import {igMedia, igData, igUser} from "../interfaces/instagram";
 import {Observable} from "rxjs";
 
@@ -8,19 +7,33 @@ import {Observable} from "rxjs";
     providedIn: "root"
 })
 export class InstagramService {
-
+    // Cutting Api Key in two parts to avoid facebook to crawl my github and cancel it.
+    // TODO : Remove the token, obviously a bad practice (but anyway this token doesn't not allow any action on my instagram page)
+    public apiKeyPartOne = "IGQVJXX3ZAVOTJqTUd2TnE2SWt1c1JZALWQzT1NNUndWNE9xMC04UGZALVzcwTWR";
+    public apiKeyPartTwo = "xdVFHdFRQRkpQTmpwd2J5VUFyZA2xrN0hpOE1ZALXBCZAWk0aFJVTGMxX2FxUC14T3dydEk0QkVrV0d3";
     constructor(private http: HttpClient) {
+        console.log(this.getApiKey());
+    }
+
+
+    /**
+     * Get Api key from server
+     * TODO : Deploy back or find workaround to store the token and refresh it every 60 days
+     */
+    public getApiKey(): string {
+        return this.apiKeyPartOne + this.apiKeyPartTwo;
+
     }
 
     /**
      * Instagram API Token Refresh (long-term, valid for 60days)
-     * TODO : Deploy back or find workaround to store the token and refresh it every 60 days
+     * TODO :Deploy back or find workaround to store the token and refresh it every 60 days
      */
     public refreshToken(): string {
         let token: string;
         const params = new HttpParams()
             .set("grant_type", "ig_refresh_token")
-            .set("access_token", firstLongLifeToken);
+            .set("access_token", this.getApiKey());
         this.http.get<{
             access_token: string
             token_type: string;
@@ -41,7 +54,7 @@ export class InstagramService {
     public getIgUserInfos(): Observable<igUser> {
         const params = new HttpParams()
             .set("fields", "id,username")
-            .set("access_token", firstLongLifeToken);
+            .set("access_token", this.getApiKey());
         return this.http.get<igUser>("https://graph.instagram.com/me", {params: params});
     }
 
@@ -51,7 +64,7 @@ export class InstagramService {
      */
     public getMediaArray(user: igUser): Observable<igData> {
         const params = new HttpParams()
-            .set("access_token", firstLongLifeToken);
+            .set("access_token", this.getApiKey());
 
         return this.http.get<igData>(`https://graph.instagram.com/v11.0/${user.id}/media`, {params: params});
     }
@@ -63,7 +76,7 @@ export class InstagramService {
     public getMediaUrl(media: igMedia): string {
         let mediaUrl: string;
         const params = new HttpParams()
-            .set("access_token", firstLongLifeToken)
+            .set("access_token", this.getApiKey())
             .set("fields", "media_url, caption");
         this.http.get<igMedia>(`https://graph.instagram.com/${media.id})`, {params: params}).subscribe((res: igMedia) => {
             mediaUrl = res.media_url;
@@ -77,7 +90,7 @@ export class InstagramService {
      */
     public getFullMedia(id:string): Observable<igMedia> {
         const params = new HttpParams()
-            .set("access_token", firstLongLifeToken)
+            .set("access_token", this.getApiKey())
             .set("fields", "media_url, caption");
 
         return this.http.get<igMedia>(`https://graph.instagram.com/${id}`, {params: params});
