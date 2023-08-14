c05707f59628f5bea123f606991ad0ebf7bc8212
MM <nerggalkore@gmail.com>
2023-08-14 15:04:19 +0200

    close burger menu on click anywhere on the page

--- angular_portfolio/src/app/header/header.component.ts
+++ angular_portfolio/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from "@angular/core";
+import {Component, ElementRef, Renderer2, ViewChild} from "@angular/core";
 import {Photo} from "../interfaces/photo";
 import {PhotosService} from "../services/photos.service";
 
@@ -8,16 +8,22 @@ import {PhotosService} from "../services/photos.service";
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent {
+export class HeaderComponent{
     @ViewChild("details") details : ElementRef;
 
     public categories: string[];
 
-    constructor(private _photoService: PhotosService) {
+    constructor(private _photoService: PhotosService,
+                private renderer: Renderer2) {
         this._photoService.getPhotos("home").subscribe((res: Photo[]) => {
             this.categories = res.map(photo => photo.category);
         });
+
+        this.renderer.listen('window', 'click',()=>{
+            this.closeDetails();
+        });
     }
+
 
     public closeDetails(): void{
         this.details.nativeElement.removeAttribute("open");
